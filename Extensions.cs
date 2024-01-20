bb4102c6e267984a55ebd4efc020a9b9fab1999c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2024-01-20 18:38:02 +0100

    ft: finalize dev logic

--- Extensions.cs
+++ Extensions.cs
@@ -1,4 +1,5 @@
-﻿using CredentialsInUrlParser.Enums;
+﻿using System.Text;
+using CredentialsInUrlParser.Enums;
 
 namespace CredentialsInUrlParser
 {
@@ -11,6 +12,28 @@ namespace CredentialsInUrlParser
         public static string ToNpgsqlConnectionString(this ConnectionParameters connectionParameters,
             bool pooling = true, PostgresSSLMode sslMode = PostgresSSLMode.Prefer,
             bool trustServerCertificate = true) =>
-            $"User ID={connectionParameters.UserName};Password={connectionParameters.Password};Server={connectionParameters.HostName};Port={connectionParameters.Port};Database={connectionParameters.Database};Pooling={pooling.ToString().ToLowerInvariant()};SSL Mode={sslMode.ToString()};Trust Server Certificate={trustServerCertificate.ToString().ToLowerInvariant()}";
+            $"User ID={connectionParameters.UserName};Password={connectionParameters.Password};Server={connectionParameters.HostName};Port={connectionParameters.Port};Database={connectionParameters.DatabasePath};Pooling={pooling.ToString().ToLowerInvariant()};SSL Mode={sslMode.ToString()};Trust Server Certificate={trustServerCertificate.ToString().ToLowerInvariant()}";
+
+        public static (string DatabaseUrl, string? DatabaseName) ToMongoConnectionSplit(
+            this ConnectionParameters connectionParameters)
+        {
+            string userInfo;
+
+            if (string.IsNullOrWhiteSpace(connectionParameters.UserName) &&
+                string.IsNullOrWhiteSpace(connectionParameters.UserName))
+                userInfo = string.Empty;
+            else
+                userInfo = $"{connectionParameters.UserName}:{connectionParameters.Password}@";
+
+            var builder =
+                new StringBuilder($"{connectionParameters.Scheme}://{userInfo}{connectionParameters.HostName}");
+
+            if (connectionParameters.Port.HasValue)
+                builder.Append($":{connectionParameters.Port}");
+
+            builder.Append(connectionParameters.AdditionalQueryParameters);
+
+            return (builder.ToString(), connectionParameters.DatabasePath);
+        }
     }
 }
